e1416b8565bffa7d692fc5bd48cd4f2371eea067
Luiz Carlos Vazo <luizcarlosvazo89@gmail.com>
2026-07-12 12:49:10 -0300

    Adicionado estilo e checkbox
    Adicionado funcionalidades de checkbox ao to-do-list e atualizado o estilo css

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// 1. Seleção de Elementos
 const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-input');
 const todoList = document.querySelector('#todo-list');
 
-// 2. Função Principal (Ouvinte de Evento)
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -19,16 +17,24 @@ todoForm.addEventListener('submit', (e) => {
 function adicionarTarefa(texto) {
 
     const li = document.createElement('li');
+    const checkbox = document.createElement('input');
     const span = document.createElement('span');
     const btnRemover = document.createElement('button');
 
+    checkbox.type = 'checkbox';
+
     span.textContent = texto;
     btnRemover.textContent = "Remover";
+    checkbox.addEventListener('change', () => {
+        span.classList.toggle('concluida', checkbox.checked);
+        li.classList.toggle('concluida', checkbox.checked);
+    });
 
     btnRemover.addEventListener('click', () => {
         li.remove();
     });
 
+    li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(btnRemover);
 
